64c2a67c04f975e71e69299392837d03454169de
ken <kentuei05@gmail.com>
2025-05-29 11:31:29 +0300

    Validate that strings match specific patterns using regular expressions

--- lib/regex.py
+++ lib/regex.py
@@ -1,13 +1,13 @@
 import re
 
-# NOTE: There are only a few tests included, so multiple solutions will work.
-# Feel free to encourage students to find oversights and add tests to this lab!
-
-name = r""
+# Name regex (already working)
+name = r"^[A-Z][a-z]*(?:[-'][A-Z][a-z]*)?(?: [A-Z][a-z]*(?:[-'][A-Z][a-z]*)?)?$"
 name_regex = re.compile(name)
 
-phone_number = r""
+# Phone number regex
+phone_number = r"^(\(\d{3}\)\s*|\d{3}[-.\s]?)\d{3}[-.\s]?\d{4}$"
 phone_regex = re.compile(phone_number)
 
-email_address = r""
+# Email regex (now disallows starting with a number)
+email_address = r"^[a-zA-Z][\w\.-]*@[\w\.-]+\.\w{2,}$"
 email_regex = re.compile(email_address)
